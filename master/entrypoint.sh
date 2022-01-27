03010ae673c2304c46af72d05d220ec52a88d2ab
dirkarnez <smalldirkalex@gmail.com>
2022-01-28 01:03:40 +0800

    - add master and node

---
+++ master/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+set -u
+
+echo "==> Executing master image entrypoint ..."
+
+echo "-> Setting up ROS"
+source "/opt/ros/noetic/setup.bash"
+
+echo "==> Container ready"
+exec "$@"
